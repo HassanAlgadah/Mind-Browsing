5f80aa49f270359c698ee71747dc76c3655230c8
Hassan Algadah <45927520+HassanAlgadah@users.noreply.github.com>
2020-07-15 13:27:05 +0300

    first

---
+++ backend/app.js
@@ -0,0 +1,26 @@
+const WebSocket = require('ws');
+const emotiv = require('./cortex_code_example')
+const express = require('express')
+const app = express()
+const socket = require('socket.io');
+
+let server = app.listen(4000,()=>console.log('emotiv is up'))
+
+let socketUrl = 'wss://localhost:6868'
+let user = {
+    "license":"",
+    "clientId":"RhvjApk4KRpG2hmlmO806LgGe2Exx9hzbX4AbbsQ",
+    "clientSecret":"RoLRROjHmzALrWrhOv0MSIhjGNkucog34LnDhEp0pkziukCkcVw4DNFgZoA46pnDHut2BlectA9PplEXEijlJpaZAdl0gjjP41jt3eOh5ulu4CoAYDbjKXzwmg7Snu3B",
+    "debit":1
+}
+let api = new emotiv.api(user,socketUrl)
+
+api.live('hassan')
+
+api.sub(['fac'])
+
+app.get('/data',(req,res)=>{
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    return res.send(JSON.parse(api.data))
+})
+
---
+++ frontend/home.js
@@ -0,0 +1,61 @@
+async function start () {
+    let eyes
+    let preveyes = null
+    let div = document.getElementById('box')
+
+    window.setInterval(()=>{
+        
+
+        getdata().then(data=>{
+            console.log(data['fac'])
+
+
+            ///////////////////////////////////////// eyes action wink right , wink left , blink
+
+            if(eyes !== data['fac'][0]) {
+                eyes = data['fac'][0]
+                console.log(eyes)
+                // right wink
+                if (eyes === 'winkR') {
+
+                }
+                
+                //left wink
+                if (eyes === 'winkL'){
+
+                }
+
+
+
+
+            }
+            ///////////////////////////////////////////////////
+
+
+
+        })
+    },10)
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+async function getdata(){
+    let request = await fetch('http://127.0.0.1:4000/data')
+    let data = await request.json()
+    return data
+}
+
+
+window.addEventListener('load', start, false);
