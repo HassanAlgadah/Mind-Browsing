0dcb53aa448a937ccd8a95a177ca9bdc135f0f84
MesharyAlsabaan <meshary.alsabaan@gmail.com>
2020-09-15 16:41:17 +0300

    Add facial expression events in python file

--- backend/python/app.py
+++ backend/python/app.py
@@ -1,32 +1,39 @@
 from flask import Flask, render_template, request, jsonify, url_for, redirect
+import keyboard
+import time
 
 app = Flask(__name__)
 
 
 @app.route('/tab')
 def press_tab():
-    return "ok"
+ keyboard.press('tab')
+ return "ok"
 
 
 @app.route('/enter')
 def press_enter():
-    return "ok"
+ keyboard.press('enter')
+ return "ok"
 
 
 @app.route('/back_page')
 def back_page():
-    return "ok"
+ keyboard.press_and_release('Alt + left arrow')
+ return "ok"
 
 
 @app.route('/refresh')
 def press_refresh():
-    return "ok"
+ keyboard.press_and_release('Ctrl + r')
+ return "ok"
 
 
 @app.route('/page_down')
 def press_page_down():
-    return "ok"
+ keyboard.press('page down')
+ return "ok"
 
 
 if __name__ == '__main__':
-    app.run()
+   app.run()
