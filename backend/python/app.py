538cb8b01b348b08ad1e2804f641ed49c9f554a2
MesharyAlsabaan <meshary.alsabaan@gmail.com>
2020-09-30 14:55:41 +0300

    mac keyboard

--- backend/python/app.py
+++ backend/python/app.py
@@ -7,30 +7,39 @@ app = Flask(__name__)
 
 @app.route('/tab')
 def press_tab():
+    time.sleep(5)
     keyboard.press('tab')
     return "ok"
 
 
 @app.route('/enter')
 def press_enter():
+    time.sleep(5)
     keyboard.press('enter')
     return "ok"
 
 
 @app.route('/back_page')
 def back_page():
-    keyboard.press_and_release('Alt + left arrow')
+    time.sleep(5)
+    #keyboard.press_and_release('Alt + left arrow')
+    #For mac
+    keyboard.press_and_release('cmd + left arrow')
     return "ok"
 
 
 @app.route('/refresh')
 def press_refresh():
-    keyboard.press_and_release('Ctrl + r')
+    time.sleep(5)
+    #keyboard.press_and_release('Ctrl + r')
+    #For mac
+    keyboard.press_and_release('cmd + r')
     return "ok"
 
 
 @app.route('/page_down')
 def press_page_down():
+    time.sleep(5)
     keyboard.press('page down')
     return "ok"
 
