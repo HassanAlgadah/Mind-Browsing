ad0c0d2ea4c60b38fed8f178d1d5484378928a60
MesharyAlsabaan <meshary.alsabaan@gmail.com>
2020-11-09 10:48:45 +0300

    LAST

--- frontend/home.js
+++ frontend/home.js
@@ -1,8 +1,8 @@
-var frown, smile, surprise, winkL, winkR;
-function getDatatFromDataBase() {
+let frown, smile, surprise, winkL, winkR;
+async function getDatatFromDataBase() {
 
-    var userId = firebase.auth().currentUser.uid;
-    var get_Data = firebase.database().ref('user/' + userId);
+    let userId = await firebase.auth().currentUser.uid;
+    let get_Data = firebase.database().ref('user/' + userId);
 
     get_Data.child("frown").on('value', snap => frown = snap.val());
     get_Data.child("smile").on('value', snap => smile = snap.val());
@@ -15,91 +15,88 @@ async function start() {
     let upperface
     let lowerface
     let eyes
-    let tab = 0, enter = 0, back_page = 0, refresh = 0, page_down = 0, timer = 0, neutral = 0
+    let surpriseCount = 0, smileCount = 0, winkLCount = 0, winkRCount = 0, frownCount = 0, timer = 0, neutral = 0
+
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            console.log('here')
+            getDatatFromDataBase().then(()=>{
+
+            window.setInterval(() => {
 
 
+                getData().then(data => {
+                    // console.log(data['fac'])
+                    //
+                    if (data['fac']) {
+
+                        eyes = data['fac'][0]
+                        upperface = data['fac'][1]
+                        lowerface = data['fac'][3]
+
+                        // right wink
+                        if (eyes === 'winkR') {
+                            winkRCount += 5
+                        }
+                        //left wink
+                        else if (eyes === 'winkL') {
+                            winkLCount += 5
+                        }
+                        //smile
+                        if (lowerface === 'smile') {
+                            smileCount++
+                        }
+
+                        //upper face
+                        if (upperface === 'surprise') {
+                            surpriseCount++
+                        } else if (upperface === 'frown') {
+                            frownCount++
+                        }
+                        if (eyes === 'neutral') neutral++
+                        if (upperface === 'neutral') neutral++
+                        if (lowerface === 'neutral') neutral++
 
 
+                        // console.log(tab, enter, back_page, refresh, page_down, timer)
 
+                        // console.log(`eyes: ${eyes}, upper face: ${upperface}, lower face: ${lowerface}`)
 
+                        if (timer % 1000 === 0) {
 
-    window.setInterval(() => {
-        getDatatFromDataBase();
-        // console.log(frown);
-        // console.log(smile);
-        // console.log(surprise);
-        // console.log(winkL);
+                            let max_fac = Math.max(surpriseCount, smileCount, winkLCount, winkRCount, frownCount, neutral / 3)
+                            // console.log(tab, enter, back_page, refresh, page_down)
+                            console.log('max: ' + max_fac, 'neutral: ' + neutral / 3)
+                            switch (max_fac) {
+                                case surpriseCount:
+                                    keyPress(surprise)
+                                    break
+                                case smileCount:
+                                    keyPress(smile)
+                                    break
+                                case winkLCount:
+                                    keyPress(winkL)
+                                    break
+                                case winkRCount:
+                                    keyPress(winkR)
+                                    break
+                                case frownCount:
+                                    keyPress(frown)
+                            }
+                            surpriseCount = 0, smileCount = 0, winkLCount = 0, winkRCount = 0, frownCount = 0, timer = 0, neutral = 0
+                        }
 
-
-
-
-        getData().then(data => {
-            // console.log(data['fac'])
-            //
-            if (data['fac']) {
-
-                eyes = data['fac'][0]
-                upperface = data['fac'][1]
-                lowerface = data['fac'][3]
-
-                // right wink
-                if (eyes === 'winkR') {
-                    refresh += 5
-                }
-                //left wink
-                else if (eyes === 'winkL') {
-                    back_page += 5
-                }
-                //smile
-                if (lowerface === 'smile') {
-                    enter++
-                }
-
-                //upper face
-                if (upperface === 'surprise') {
-                    tab++
-                } else if (upperface === 'frown') {
-                    page_down++
-                }
-                if (eyes === 'neutral') neutral++
-                if (upperface === 'neutral') neutral++
-                if (lowerface === 'neutral') neutral++
-
-
-                // console.log(tab, enter, back_page, refresh, page_down, timer)
-
-                // console.log(`eyes: ${eyes}, upper face: ${upperface}, lower face: ${lowerface}`)
-
-                if (timer % 1000 === 0) {
-
-                    let max_fac = Math.max(tab, enter, back_page, refresh, page_down, neutral / 3)
-                    // console.log(tab, enter, back_page, refresh, page_down)
-                    console.log('max: ' + max_fac, 'neutral: ' + neutral / 3)
-                    switch (max_fac) {
-                        case tab:
-                            keyPress('tab')
-                            break
-                        case enter:
-                            keyPress('enter')
-                            break
-                        case back_page:
-                            keyPress('back_page')
-                            break
-                        case refresh:
-                            keyPress('refresh')
-                            break
-                        case page_down:
-                            keyPress('page_down')
+                        timer += 10
                     }
-                    tab = 0, enter = 0, back_page = 0, refresh = 0, page_down = 0, timer = 0, neutral = 0
-                }
 
-                timer += 10
-            }
+                })
 
+            }, 10)
         })
+        } else { console.log('erooooor')
+        }
+    })
 
-    }, 10)
 
 
 }
